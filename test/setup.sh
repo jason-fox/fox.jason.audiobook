8075fd877ea8841842d9904c89a1bf2aa88c6703
Jason Fox <jason.fox@fiware.org>
2020-12-07 21:35:28 +0100

    Switch to Alpine

--- test/setup.sh
+++ test/setup.sh
@@ -1,4 +1,17 @@
-add-apt-repository ppa:mc3man/trusty-media
-apt-get update
-apt-get install ffmpeg
+apk add --update build-base curl nasm tar bzip2 \
+  zlib-dev openssl-dev yasm-dev lame-dev libogg-dev x264-dev libvpx-dev libvorbis-dev x265-dev freetype-dev libass-dev libwebp-dev rtmpdump-dev libtheora-dev opus-dev && \
+
+  DIR=$(mktemp -d) && cd ${DIR} && \
+
+  curl -s http://ffmpeg.org/releases/ffmpeg-3.0.2.tar.gz | tar zxvf - -C . && \
+  cd ffmpeg-3.0.2 && \
+  ./configure \
+  --enable-version3 --enable-gpl --enable-nonfree --enable-small --enable-libmp3lame --enable-libx264 --enable-libx265 --enable-libvpx --enable-libtheora --enable-libvorbis --enable-libopus --enable-libass --enable-libwebp --enable-librtmp --enable-postproc --enable-avresample --enable-libfreetype --enable-openssl --disable-debug && \
+  make && \
+  make install && \
+  make distclean && \
+
+  rm -rf ${DIR} && \
+  rm -rf /var/cache/apk/*
+
 ffmpeg -version
